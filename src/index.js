48baf7c19c91af6844b34bfad0fd0d0583d59971
Lucas Machado <lmachado72@outlook.com>
2021-02-21 12:07:44 -0300

    List /repositories

--- src/index.js
+++ src/index.js
@@ -8,14 +8,24 @@ app.use(express.json());
 
 const repositories = [];
 
+app.get('/repositories', (req, res) => {
+  const { title } = req.query;
+
+  const results = title
+    ? repositories.filter(repo => repo.title.includes(title))
+    : repositories;
+
+  return res.json(results);
+});
+
 app.post('/repositories', (req, res) => {
   const { title, url, techs } = req.body;
 
-  const repository = { id: uuid(), title, url, techs }
+  const repo = { id: uuid(), title, url, techs }
 
-  repositories.push(repository);
+  repositories.push(repo);
 
-  return res.json(repository);
+  return res.json(repo);
 });
 
  app.listen(PORT, () => {
