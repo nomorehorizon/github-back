503bf8da50b8f20346901f764cc8e29bf43dca2b
Lucas Machado <lmachado72@outlook.com>
2021-02-21 11:58:48 -0300

    Create /repositories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
- const express = require('express');
+const express = require('express');
+const { uuid } = require('uuidv4');
 
- const app = express();
- const PORT = 3333;
+const app = express();
+const PORT = 3333;
+
+app.use(express.json());
+
+const repositories = [];
+
+app.post('/repositories', (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = { id: uuid(), title, url, techs }
+
+  repositories.push(repository);
+
+  return res.json(repository);
+});
 
  app.listen(PORT, () => {
    console.log('👾  Back-end started!')
